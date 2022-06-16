d27d33010be840066dd86c4210a258f50d263ffb
noothanks <coreyfuller727@yahoo.com>
2022-06-16 17:44:53 -0400

    adding validation to user response

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub username'
+        message: 'Enter your GitHub username',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your github username!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
@@ -33,12 +49,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your project name!');
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your project description!');
+                  return false;
+                }
+              }
         },
         {
             type: 'checkbox',
@@ -49,7 +81,15 @@ const promptUser = () => {
         {
             tye: 'input',
             name: 'link',
-            message: 'Enter a GitHub link to your project'
+            message: 'Enter a GitHub link to your project',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your project link!');
+                  return false;
+                }
+              }
         },
         {
             type: 'confirm',
