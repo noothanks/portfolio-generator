51406aeac30af1afcd5f9c0a9ebf8f247d717ca1
noothanks <coreyfuller727@yahoo.com>
2022-06-16 16:51:59 -0400

    generating webpage and modulating function

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-  console.log('================');
+//   console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out the index.html to see the output');
+});
