0b3b97e8d08b6e2d56478af6dcc709321078a094
noothanks <coreyfuller727@yahoo.com>
2022-06-16 17:14:12 -0400

    installing inquirer and setting up prompts

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+inquirer .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(
+    answers => console.log(answers)
+);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// const pageHTML = generatePage(name, github);
 
-//   console.log('================');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out the index.html to see the output');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
